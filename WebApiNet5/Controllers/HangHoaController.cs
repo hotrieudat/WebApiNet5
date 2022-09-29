304b59f30304a14ce994fe56d9cabf424ffbfede
h-dat <h-dat@plott.co.jp>
2022-09-29 13:48:37 +0700

    Restfull API

---
+++ WebApiNet5/Controllers/HangHoaController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebApiNet5.Model;
+
+namespace WebApiNet5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(p => p.MaHangHoa == id);
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return Ok(hangHoa);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Create(HangHoaVM hangHoaVM)
+        {
+            var hangHoa = new HangHoa()
+            {
+                MaHangHoa = Guid.NewGuid().ToString(),
+                TenHangHoa = hangHoaVM.TenHangHoa,
+                DonGia = hangHoaVM.DonGia
+            };
+
+            hangHoas.Add(hangHoa);
+
+            return Ok(new
+            {
+                Success = true,
+                Data = hangHoa, 
+            });
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, HangHoa hangHoaEdit)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(p => p.MaHangHoa == id);
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+
+                if (id != hangHoa.MaHangHoa)
+                {
+                    return BadRequest();
+                }
+
+                hangHoa.TenHangHoa = hangHoaEdit.TenHangHoa;
+                hangHoa.DonGia = hangHoaEdit.DonGia;
+                return Ok();
+                
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("id")]
+        public IActionResult Remove(string id)
+        {
+            try
+            {
+                var hangHoa = hangHoas.SingleOrDefault(p => p.MaHangHoa == id);
+
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
---
+++ WebApiNet5/Model/HangHoa.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApiNet5.Model
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa : HangHoaVM
+    {
+        public string MaHangHoa { get; set; }
+    }
+}
